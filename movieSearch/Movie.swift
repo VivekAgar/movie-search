44dcc66b3005d379fe659d53dfc70f22b371829d
Satya <Satya@androids-MacBook-Pro.local>
2016-07-30 17:50:04 +0530

    add models

---
+++ movieSearch/Movie.swift
@@ -0,0 +1,33 @@
+//
+//  Movie.swift
+//  movieSearchDummy
+//
+//  Created by Satya on 28/07/16.
+//  Copyright © 2016 slonkit. All rights reserved.
+//
+
+import Foundation
+
+class Movie : NSObject{
+    
+    var title : String?
+    var year : String?
+    var rated : String?
+    var released : NSDate?
+    var runTime : String?
+    var genre : String?
+    var director : String?
+    var writer : String?
+    var actors : String?
+    var plot : String?
+    var language : String?
+    var country : String?
+    var awards : String?
+    var posterUrl : String?
+    var metascore : String?
+    var imdbRating : Float?
+    var imdbVotes : Float?
+    var imdbID : String?
+    var type : String?
+    
+}
---
+++ movieSearch/MovieSearchResult.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchResult.swift
+//  movieSearchDummy
+//
+//  Created by Satya on 28/07/16.
+//  Copyright © 2016 slonkit. All rights reserved.
+//
+
+import Foundation
+
+class MovieSearchResult : NSObject{
+    
+    //Not Used For Now
+    var searchResultID : String?
+    //count of search results
+    var totalsearchResults : NSNumber?
+    var searchResultType : String?
+    var movies : NSArray?
+    //search string
+    var searchString :String?
+    //current Page Index
+    var currentPageIndex : NSInteger?
+    
+}
